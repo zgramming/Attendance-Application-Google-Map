ac6fae3b89ee925530f312aed342f8fb3f920f60
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-03 23:15:28 +0700

    Create Function Calculate Distance

--- lib/src/function/common_function.dart
+++ lib/src/function/common_function.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:global_template/global_template.dart';
 import 'package:location_permissions/location_permissions.dart' as lc;
 import 'package:location/location.dart';
+import 'package:great_circle_distance2/great_circle_distance2.dart';
 
 class CommonFunction {
   Location location = new Location();
@@ -86,6 +87,62 @@ class CommonFunction {
       }
     }
     return false;
+  }
+
+  double getDistanceLocation(
+      double latitude1, double longitude1, double latitude2, double longitude2,
+      {int typeCalculate = 0, GreatCircleDistance typeGCD}) {
+    GreatCircleDistance calculate;
+
+    if (calculate == null) {
+      calculate = GreatCircleDistance.fromDegrees(
+        latitude1: latitude1,
+        longitude1: longitude1,
+        latitude2: latitude2,
+        longitude2: longitude2,
+      );
+    } else {
+      if (calculate == GreatCircleDistance.fromDegrees()) {
+        calculate = GreatCircleDistance.fromDegrees(
+          latitude1: latitude1,
+          longitude1: longitude1,
+          latitude2: latitude2,
+          longitude2: longitude2,
+        );
+      } else {
+        calculate = GreatCircleDistance.fromRadians(
+          latitude1: latitude1,
+          longitude1: longitude1,
+          latitude2: latitude2,
+          longitude2: longitude2,
+        );
+      }
+    }
+
+    var result;
+    if (typeCalculate == 0) {
+      result = calculate.haversineDistance();
+    } else if (typeCalculate == 1) {
+      result = calculate.sphericalLawOfCosinesDistance();
+    } else if (typeCalculate == 2) {
+      result = calculate.vincentyDistance();
+    } else {
+      result = calculate.haversineDistance();
+    }
+    return result;
+  }
+
+  Color changeColorRadius(
+    double distanceTwoLocation,
+    double radius, {
+    Color outsideRadius = Colors.purple,
+    Color insideRadius = Colors.green,
+  }) {
+    if (distanceTwoLocation < radius) {
+      return insideRadius;
+    } else {
+      return outsideRadius;
+    }
   }
 }
 
