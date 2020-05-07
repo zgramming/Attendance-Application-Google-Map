e8f66f2b11a5123c292b997ba8279ebca7d9d9ba
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-07 20:51:53 +0700

    Checking User in Splashscreen

--- lib/src/ui/screens/splash_screen.dart
+++ lib/src/ui/screens/splash_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:global_template/global_template.dart';
+import 'package:provider/provider.dart';
 
+import './welcome_screen.dart';
 import './login_screen.dart';
+
+import '../../providers/user_provider.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
@@ -15,7 +19,8 @@ class SplashScreen extends StatelessWidget {
             size: sizes.height(context) / 4,
           ),
         ),
-        navigateAfterSplashScreen: LoginScreen(),
+        navigateAfterSplashScreen: Consumer<UserProvider>(
+            builder: (_, value, __) => value.user.idUser == null ? LoginScreen() : WelcomeScreen()),
       ),
     );
   }
