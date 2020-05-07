856ea141252fecc3634fa88f99bee6d3c6a33aa3
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-07 20:49:52 +0700

    Base Api Url

--- global_template/lib/variable/config/app_config.dart
+++ global_template/lib/variable/config/app_config.dart
@@ -1,13 +1,15 @@
 class AppConfig {
   String urlApk = '';
 
-  Map<String, String> headersApi = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
-  String baseImageApiUrl = '';
-  String baseFilesApiUrl = '';
-  String baseApiUrl = '';
+  Map<String, String> headersApi = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+  String baseImageApiUrl = 'https://flutter-absensi.000webhostapp.com/zabsenin/images/';
+  String baseFilesApiUrl = 'https://flutter-absensi.000webhostapp.com/zabsenin/files/';
+  String baseApiUrl = 'https://flutter-absensi.000webhostapp.com/zabsenin/api/';
+
   String indonesiaLocale = 'id_ID';
+
+  final String userController = "user_controller";
 }
 
 final appConfig = AppConfig();
