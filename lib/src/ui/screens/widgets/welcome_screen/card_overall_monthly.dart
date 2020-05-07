a8f923267382bc7ca3754507bd066b45c68100ce
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-07 20:53:05 +0700

    Remove Stack Image Profil

--- lib/src/ui/screens/widgets/welcome_screen/card_overall_monthly.dart
+++ lib/src/ui/screens/widgets/welcome_screen/card_overall_monthly.dart
@@ -8,51 +8,36 @@ class CardOverallMonthly extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: sizes.height(context) / 3,
-      margin: EdgeInsets.only(top: sizes.height(context) / 12),
+      margin: EdgeInsets.only(top: sizes.height(context) / 10),
       // color: Colors.purple,
-      child: Stack(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(
-              top: sizes.height(context) / 30,
-              bottom: sizes.height(context) / 30,
-              left: sizes.width(context) / 20,
-              right: sizes.width(context) / 20,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(
+          top: sizes.height(context) / 30,
+          bottom: sizes.height(context) / 30,
+          left: sizes.width(context) / 20,
+          right: sizes.width(context) / 20,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: colorPallete.primaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: colorPallete.black.withOpacity(.5),
+              offset: Offset(1.5, 3),
+              blurRadius: 2,
             ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: colorPallete.primaryColor,
-              boxShadow: [
-                BoxShadow(
-                  color: colorPallete.black.withOpacity(.5),
-                  offset: Offset(1.5, 3),
-                  blurRadius: 2,
-                ),
-              ],
-            ),
-            child: Container(
-              padding: const EdgeInsets.only(top: 8.0, left: 12.0),
-              child: Column(
-                children: [
-                  buildHeader(context),
-                  buildBody(context),
-                ],
-              ),
-            ),
+          ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.only(top: 8.0, left: 12.0),
+          child: Column(
+            children: [
+              buildHeader(context),
+              buildBody(context),
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.only(
-              left: 8.0,
-            ),
-            child: ShowImageNetwork(
-              imageUrl: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png",
-              fit: BoxFit.cover,
-              imageWidth: 5,
-              imageHeight: 8.25,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
