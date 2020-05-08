95fa71679f06945cad988db591ede1b2fe1a470e
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-08 16:51:23 +0700

    Calendar Horizontal Improve To Statefull

--- lib/src/ui/screens/widgets/welcome_screen/calendar_horizontal.dart
+++ lib/src/ui/screens/widgets/welcome_screen/calendar_horizontal.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:global_template/global_template.dart';
 
-class CalendarHorizontal extends StatelessWidget {
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+class CalendarHorizontal extends StatefulWidget {
   final DateTime networkDateTime;
   CalendarHorizontal({@required this.networkDateTime});
+
+  @override
+  _CalendarHorizontalState createState() => _CalendarHorizontalState();
+}
+
+class _CalendarHorizontalState extends State<CalendarHorizontal> {
+  final ItemScrollController itemScrollController = ItemScrollController();
+
+  /// Listener that reports the position of items when the list is scrolled.
+  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -11,7 +28,7 @@ class CalendarHorizontal extends StatelessWidget {
         children: [
           SizedBox(height: 10),
           Text(
-            globalF.formatYearMonth(networkDateTime, type: 3),
+            globalF.formatYearMonth(widget.networkDateTime, type: 3),
             style: appTheme.headline6(context),
             textAlign: TextAlign.center,
           ),
@@ -19,57 +36,66 @@ class CalendarHorizontal extends StatelessWidget {
           Container(
             height: sizes.height(context) / 8,
             margin: const EdgeInsets.symmetric(horizontal: 16),
-            // color: Colors.green,
-            child: ListView.builder(
+            child: ScrollablePositionedList.builder(
+              initialScrollIndex: widget.networkDateTime.day,
+              initialAlignment: 0.6,
+              itemCount: globalF.totalDaysOfMonth(
+                widget.networkDateTime.year,
+                widget.networkDateTime.month,
+              ),
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
               scrollDirection: Axis.horizontal,
-              itemExtent: 90,
-              itemCount: globalF.totalDaysOfMonth(networkDateTime.month, networkDateTime.year),
               itemBuilder: (BuildContext context, int index) {
-                String getDaysName = globalF
-                    .formatDay(DateTime(networkDateTime.year, networkDateTime.month, index + 1));
-                return Card(
-                  color: isWeekend(getDaysName),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    side: networkDateTime.day == index + 1
-                        ? BorderSide(color: Colors.green, width: 3)
-                        : BorderSide(color: Colors.transparent),
-                  ),
-                  child: InkWell(
-                    onTap: () => '',
-                    borderRadius: BorderRadius.circular(15),
-                    child: Column(
-                      children: [
-                        Flexible(
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "${index + 1}",
-                              style: appTheme.headline4(context).copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    color: isWeekend(
-                                      getDaysName,
-                                      colorWeekend: colorPallete.white,
-                                      colorWeekDay: colorPallete.black.withOpacity(.7),
+                String getDaysName = globalF.formatDay(
+                    DateTime(widget.networkDateTime.year, widget.networkDateTime.month, index + 1));
+                print(globalF.totalWeekDayOfMonth(12, 2019));
+                return SizedBox(
+                  width: sizes.width(context) / 4.5,
+                  child: Card(
+                    color: isWeekend(getDaysName),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: widget.networkDateTime.day == index + 1
+                          ? BorderSide(color: Colors.green, width: 3)
+                          : BorderSide(color: Colors.transparent),
+                    ),
+                    child: InkWell(
+                      onTap: () => '',
+                      borderRadius: BorderRadius.circular(15),
+                      child: Column(
+                        children: [
+                          Flexible(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "${index + 1}",
+                                style: appTheme.headline4(context).copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: isWeekend(
+                                        getDaysName,
+                                        colorWeekend: colorPallete.white,
+                                        colorWeekDay: colorPallete.black.withOpacity(.7),
+                                      ),
                                     ),
-                                  ),
+                              ),
                             ),
+                            fit: FlexFit.tight,
+                            flex: 3,
                           ),
-                          fit: FlexFit.tight,
-                          flex: 3,
-                        ),
-                        Flexible(
-                          child: Text(
-                            globalF.formatDay(
-                                DateTime(networkDateTime.year, networkDateTime.month, index + 1)),
-                            textAlign: TextAlign.center,
-                            style: appTheme.caption(context).copyWith(
-                                color: isWeekend(getDaysName, colorWeekend: colorPallete.white)),
+                          Flexible(
+                            child: Text(
+                              globalF.formatDay(DateTime(widget.networkDateTime.year,
+                                  widget.networkDateTime.month, index + 1)),
+                              textAlign: TextAlign.center,
+                              style: appTheme.caption(context).copyWith(
+                                  color: isWeekend(getDaysName, colorWeekend: colorPallete.white)),
+                            ),
+                            fit: FlexFit.tight,
                           ),
-                          fit: FlexFit.tight,
-                        ),
-                      ],
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                      ),
                     ),
                   ),
                 );
