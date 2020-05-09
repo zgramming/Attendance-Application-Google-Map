846a955f6057f62f895fef3f65392ff511181434
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-09 16:02:24 +0700

    New Widget User Profile

---
+++ lib/src/ui/screens/widgets/welcome_screen/user_profile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:global_template/global_template.dart';
+import 'package:provider/provider.dart';
+import '../../../../providers/user_provider.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Container(
+        child: Column(
+          children: [
+            const SizedBox(height: kToolbarHeight),
+            Text(
+              'Selamat Datang',
+              style: appTheme
+                  .headline5(context)
+                  .copyWith(fontFamily: 'Righteous', color: colorPallete.black),
+            ),
+            const SizedBox(height: 10),
+            Consumer<UserProvider>(
+              builder: (_, value, __) => Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      value.user.fullName,
+                      style: appTheme.subtitle2(context),
+                    ),
+                  ),
+                  ShowImageNetwork(
+                    imageUrl: "${appConfig.baseImageApiUrl}/user/${value.user.image}",
+                    isCircle: true,
+                  )
+                ],
+              ),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
