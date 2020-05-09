2ea534317169d96d8fe7294320206c95dd75b2a4
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-09 16:01:21 +0700

    Implement AppBarAnimatedColor

--- lib/src/ui/screens/welcome_screen.dart
+++ lib/src/ui/screens/welcome_screen.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:location/location.dart';
 import 'package:global_template/global_template.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../screens/maps_screen.dart';
 
 import '../../providers/zabsen_provider.dart';
-import '../../function/common_function.dart';
+import '../../function/zabsen_function.dart';
 
+import './widgets/welcome_screen/fab.dart';
+import './widgets/welcome_screen/user_profile.dart';
+import './widgets/welcome_screen/table_attendance.dart';
 import './widgets/welcome_screen/button_attendance.dart';
 import './widgets/welcome_screen/calendar_horizontal.dart';
-import './widgets/welcome_screen/table_attendance.dart';
 import './widgets/welcome_screen/card_overall_monthly.dart';
+import './widgets/welcome_screen/animation/appbar_animated_color.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const routeNamed = "/welcome-screen";
@@ -25,11 +28,13 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen>
     with TickerProviderStateMixin, WidgetsBindingObserver {
   AnimationController _hideFloatingButton;
+  AnimationController _appbarController;
   @override
   void initState() {
     commonF.initPermission(context);
     WidgetsBinding.instance.addObserver(this);
     _hideFloatingButton = AnimationController(vsync: this, duration: kThemeAnimationDuration);
+    _appbarController = AnimationController(vsync: this, duration: Duration(milliseconds: 100));
     super.initState();
   }
 
@@ -60,6 +65,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     _hideFloatingButton.dispose();
+    _appbarController.dispose();
     super.dispose();
   }
 
@@ -69,60 +75,63 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       onNotification: (notification) => commonF.handleScrollNotification(
         notification,
         controllerButton: _hideFloatingButton,
+        appBarController: _appbarController,
       ),
-      child: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CardOverallMonthly(),
-                  Consumer2<ZAbsenProvider, GlobalProvider>(
-                    builder: (_, value, value2, __) {
-                      return AnimatedCrossFade(
-                        firstChild: TableAttendance(),
-                        secondChild: CalendarHorizontal(
-                          networkDateTime: value.networkDateTime ?? DateTime.now(),
-                        ),
-                        duration: Duration(seconds: 3),
-                        crossFadeState: value2.isChangeMode
-                            ? CrossFadeState.showFirst
-                            : CrossFadeState.showSecond,
-                        firstCurve: Curves.decelerate,
-                        secondCurve: Curves.decelerate,
-                      );
-                    },
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+            statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark),
+        child: SafeArea(
+          child: Scaffold(
+            body: Stack(
+              fit: StackFit.expand,
+              children: [
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      UserProfile(),
+                      CardOverallMonthly(),
+                      Consumer<GlobalProvider>(
+                        builder: (_, value2, __) {
+                          return AnimatedCrossFade(
+                            firstChild: TableAttendance(),
+                            secondChild: Container(
+                              child: CalendarHorizontal(),
+                            ),
+                            duration: Duration(seconds: 1),
+                            crossFadeState: value2.isChangeMode
+                                ? CrossFadeState.showFirst
+                                : CrossFadeState.showSecond,
+                            firstCurve: Curves.decelerate,
+                            secondCurve: Curves.fastOutSlowIn,
+                          );
+                        },
+                      ),
+                      SizedBox(height: 1000),
+                    ],
                   ),
-                  SizedBox(height: 1000),
-                ],
-              ),
+                ),
+                Consumer<ZAbsenProvider>(
+                  builder: (_, absenProvider, __) => ButtonAttendance(
+                    right: 60,
+                    hideFabAnimation: _hideFloatingButton,
+                    onTapAttendence: () {
+                      absenProvider.getCurrentPosition().then(
+                            (_) => Navigator.of(context).pushNamed(MapScreen.routeNamed),
+                          );
+                    },
+                    onTapGoHome: () => print('go home'),
+                  ),
+                ),
+                Positioned(
+                  child: AppBarAnimatedColor(controller: _appbarController),
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                ),
+              ],
             ),
-            Consumer<ZAbsenProvider>(
-              builder: (_, absenProvider, __) => ButtonAttendance(
-                // left: 20,
-                right: 60,
-                hideFabAnimation: _hideFloatingButton,
-                onTapAttendence: () {
-                  absenProvider.getCurrentPosition().then(
-                        (_) => Navigator.of(context).pushNamed(MapScreen.routeNamed),
-                      );
-                },
-                onTapGoHome: () => print('go home'),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: Consumer<GlobalProvider>(
-          builder: (_, value, __) => FloatingActionButton(
-            onPressed: () {
-              value.setChangeMode(value.isChangeMode);
-              print(value.isChangeMode);
-            },
-            mini: true,
-            child: Icon(
-                value.isChangeMode ? FontAwesomeIcons.calendarTimes : FontAwesomeIcons.tabletAlt),
+            floatingActionButton: FabChangeMode(),
           ),
         ),
       ),
