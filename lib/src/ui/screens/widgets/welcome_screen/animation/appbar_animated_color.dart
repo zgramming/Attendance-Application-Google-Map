e3b9d4cedafc3cced643e9e380e30af9c3565475
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-09 16:02:50 +0700

    New Animation AppBarAnimatedColor

---
+++ lib/src/ui/screens/widgets/welcome_screen/animation/appbar_animated_color.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:global_template/global_template.dart';
+
+class AppBarAnimatedColor extends StatefulWidget {
+  final Widget title;
+  final AnimationController controller;
+  AppBarAnimatedColor({@required this.controller, this.title});
+  @override
+  _AppBarAnimatedColorState createState() => _AppBarAnimatedColorState();
+}
+
+class _AppBarAnimatedColorState extends State<AppBarAnimatedColor> {
+  Animation<Color> appBarColor, iconColor;
+
+  @override
+  void initState() {
+    appBarColor = ColorTween(begin: Colors.transparent, end: colorPallete.primaryColor)
+        .animate(widget.controller);
+    iconColor = ColorTween(begin: colorPallete.accentColor, end: colorPallete.white)
+        .animate(widget.controller);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.controller,
+      builder: (_, __) => AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FlutterLogo(),
+        ),
+        elevation: 0,
+        title: widget.title ??
+            Text(
+              '',
+              style: appTheme.subtitle1(context).copyWith(color: iconColor.value),
+            ),
+        backgroundColor: appBarColor.value,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: InkWell(
+              onTap: () => '',
+              child: Icon(
+                FontAwesomeIcons.bars,
+                color: iconColor.value,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
