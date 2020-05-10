749b537fb98514b8b49a0223630a1efef6f051f8
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-10 21:00:41 +0700

    Adding property Linear & circular indicator

--- global_template/lib/widgets/reusable/futurebuilder_loading.dart
+++ global_template/lib/widgets/reusable/futurebuilder_loading.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class LoadingFutureBuilder extends StatelessWidget {
+  final bool isLinearProgressIndicator;
+  LoadingFutureBuilder({this.isLinearProgressIndicator = false});
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    return Center(
+        child: isLinearProgressIndicator ? LinearProgressIndicator() : CircularProgressIndicator());
   }
 }
