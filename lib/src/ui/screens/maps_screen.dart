95ef2112a18a7053113a88b1be73eb640c7e1d46
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-10 21:02:01 +0700

    Function To Absen

--- lib/src/ui/screens/maps_screen.dart
+++ lib/src/ui/screens/maps_screen.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
-import 'package:flutter/services.dart';
 
+import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:global_template/global_template.dart';
 import 'package:location/location.dart';
+import 'package:network/network.dart';
 import 'package:provider/provider.dart';
+import 'package:global_template/global_template.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import './widgets/welcome_screen/button_attendance.dart';
 
 import '../../function/zabsen_function.dart';
 import '../../providers/zabsen_provider.dart';
-
-import './widgets/live_clock.dart';
+import '../../providers/user_provider.dart';
 
 class MapScreen extends StatefulWidget {
   static const routeNamed = "/map-screen";
@@ -23,12 +26,11 @@ class _MapScreenState extends State<MapScreen> {
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController mapController;
 
-  double tujuanLatitude = -6.1767733;
-  double tujuanLongitude = 106.9955114;
-  double radiusCircle = 3.5;
+  double radiusCircle = 500;
+
   void getLocation(ZAbsenProvider provider) async {
     Location location = Location();
-    location.changeSettings(interval: 1000);
+    location.changeSettings(interval: 3000);
     location.onLocationChanged.listen((currentLocation) {
       if (currentLocation == null) {
         return null;
@@ -52,6 +54,8 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print("Rebuild Maps Screen");
+
     return WillPopScope(
       onWillPop: () async => false,
       child: AnnotatedRegion<SystemUiOverlayStyle>(
@@ -67,23 +71,28 @@ class _MapScreenState extends State<MapScreen> {
                       final distanceTwoLocation = commonF.getDistanceLocation(
                         absenProvider.currentPosition.latitude,
                         absenProvider.currentPosition.longitude,
-                        tujuanLatitude,
-                        tujuanLongitude,
+                        absenProvider.destinasiModel.latitude,
+                        absenProvider.destinasiModel.longitude,
                       );
                       print(
                           "Jarak $distanceTwoLocation || Lokasi Saya ${absenProvider.currentPosition}");
                       return GoogleMap(
-                        circles: Set.of({
-                          Circle(
-                            circleId: CircleId('1'),
-                            strokeColor: Colors.transparent,
-                            fillColor: commonF
-                                .changeColorRadius(distanceTwoLocation, radiusCircle)
-                                .withOpacity(.8),
-                            center: LatLng(tujuanLatitude, tujuanLongitude),
-                            radius: radiusCircle,
-                          ),
-                        }),
+                        circles: Set.of(
+                          {
+                            Circle(
+                              circleId: CircleId('1'),
+                              strokeColor: Colors.transparent,
+                              fillColor: commonF
+                                  .changeColorRadius(distanceTwoLocation, radiusCircle)
+                                  .withOpacity(.8),
+                              center: LatLng(
+                                absenProvider.destinasiModel.latitude,
+                                absenProvider.destinasiModel.longitude,
+                              ),
+                              radius: radiusCircle,
+                            ),
+                          },
+                        ),
                         initialCameraPosition: CameraPosition(
                           target: LatLng(absenProvider.currentPosition.latitude,
                               absenProvider.currentPosition.longitude),
@@ -95,69 +104,22 @@ class _MapScreenState extends State<MapScreen> {
                           _controller.complete(controller);
                         },
                         myLocationEnabled: true,
-                        // myLocationButtonEnabled: true,
                       );
                     },
                   ),
                   Positioned(
-                    child: Card(
-                      // color: Colors.transparent,
-                      elevation: 0,
-                      child: Column(
-                        children: [
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Text("Absen Datang"),
-                              Text('Absen Pulang'),
-                            ],
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          ),
-                          SizedBox(height: 5),
-                          Consumer<ZAbsenProvider>(
-                            builder: (_, absenProvider, __) {
-                              final distanceTwoLocation = commonF.getDistanceLocation(
-                                absenProvider.currentPosition.latitude,
-                                absenProvider.currentPosition.longitude,
-                                tujuanLatitude,
-                                tujuanLongitude,
-                              );
-                              return Row(
-                                children: [
-                                  Flexible(
-                                    child: ButtonCustom(
-                                      onPressed:
-                                          distanceTwoLocation < radiusCircle ? () => '' : null,
-                                      padding: EdgeInsets.symmetric(horizontal: 6.0),
-                                      child: LiveClock(),
-                                    ),
-                                  ),
-                                  Flexible(
-                                    child: ButtonCustom(
-                                      padding: EdgeInsets.symmetric(horizontal: 6.0),
-                                      child: LiveClock(),
-                                      onPressed:
-                                          distanceTwoLocation < radiusCircle ? () => '' : null,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                          SizedBox(height: 5),
-                        ],
-                      ),
-                    ),
                     bottom: 30,
                     left: 10,
                     right: 50,
+                    child: ButtonAttendance(
+                      onTapAbsen: () => _validateAbsenMasuk(radiusCircle),
+                      backgroundColor: Colors.transparent,
+                      onTapPulang: () => _validateAbsenPulang(radiusCircle),
+                    ),
                   ),
                   Positioned(
                     child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                        // realTimePosition.cancel();
-                      },
+                      onTap: () => Navigator.of(context).pop(),
                       child: CircleAvatar(
                         backgroundColor: colorPallete.white,
                         child: Icon(FontAwesomeIcons.times),
@@ -173,5 +135,64 @@ class _MapScreenState extends State<MapScreen> {
         ),
       ),
     );
+  }
+
+  void _validateAbsenMasuk(double radius) async {
+    final absenProvider = context.read<ZAbsenProvider>();
+    final userProvider = context.read<UserProvider>();
+    final trueTime = await commonF.getTrueTime();
+    final timeFormat = DateFormat("HH:mm:ss").format(trueTime);
+    final distanceTwoLocation = commonF.getDistanceLocation(
+      absenProvider.currentPosition.latitude,
+      absenProvider.currentPosition.longitude,
+      absenProvider.destinasiModel.latitude,
+      absenProvider.destinasiModel.longitude,
+    );
+    final isUserInsideRadius = commonF.isInsideRadiusCircle(distanceTwoLocation, radius);
+    if (isUserInsideRadius) {
+      try {
+        final result = await absensiAPI.absensiMasuk(
+          idUser: userProvider.user.idUser,
+          tanggalAbsen: trueTime,
+          tanggalAbsenMasuk: trueTime,
+          jamAbsenMasuk: timeFormat,
+          createdDate: trueTime,
+        );
+
+        globalF.showToast(message: result, isSuccess: true, isLongDuration: true);
+        Navigator.of(context).pop();
+      } catch (e) {
+        globalF.showToast(message: e, isError: true);
+      }
+    } else {
+      globalF.showToast(message: "Anda Diluar Jangkauan Absensi", isError: true);
+    }
+  }
+
+  void _validateAbsenPulang(double radius) async {
+    final absenProvider = context.read<ZAbsenProvider>();
+    final userProvider = context.read<UserProvider>();
+    final trueTime = await commonF.getTrueTime();
+    final timeFormat = DateFormat("HH:mm:ss").format(trueTime);
+    final distanceTwoLocation = commonF.getDistanceLocation(
+      absenProvider.currentPosition.latitude,
+      absenProvider.currentPosition.longitude,
+      absenProvider.destinasiModel.latitude,
+      absenProvider.destinasiModel.longitude,
+    );
+
+    final isUserInsideRadius = commonF.isInsideRadiusCircle(distanceTwoLocation, radius);
+    if (isUserInsideRadius) {
+      final result = await absensiAPI.absensiPulang(
+        idUser: userProvider.user.idUser,
+        tanggalAbsenPulang: trueTime,
+        jamAbsenPulang: timeFormat,
+        updateDate: trueTime,
+      );
+      globalF.showToast(message: result, isSuccess: true, isLongDuration: true);
+      Navigator.of(context).pop();
+    } else {
+      globalF.showToast(message: "Anda Diluar Jangkauan Absensi", isError: true);
+    }
   }
 }
