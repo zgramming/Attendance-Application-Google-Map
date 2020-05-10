f5bedc3c73f88b31c344188b60d4e676d66ce442
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-10 21:04:29 +0700

    Export To Network Packages

--- network/lib/network.dart
+++ network/lib/network.dart
@@ -2,3 +2,9 @@ library network;
 
 export './model/user_model.dart';
 export './api/user_api.dart';
+
+export './model/absensi_model.dart';
+export './api/absensi_api.dart';
+
+export './model/destinasi_model.dart';
+export './api/destinasi_api.dart';
