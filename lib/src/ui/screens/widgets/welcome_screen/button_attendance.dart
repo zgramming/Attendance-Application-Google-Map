1ca1938d0b495203a04602380b9db2827b12c44c
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-10 21:02:57 +0700

    Adding Logic To Button Attendance

--- lib/src/ui/screens/widgets/welcome_screen/button_attendance.dart
+++ lib/src/ui/screens/widgets/welcome_screen/button_attendance.dart
@@ -1,67 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:global_template/global_template.dart';
+import 'package:network/network.dart';
+import 'package:provider/provider.dart';
+
 import '../live_clock.dart';
+import '../../maps_screen.dart';
+import '../../../../providers/user_provider.dart';
+import '../../../../providers/zabsen_provider.dart';
 
-class ButtonAttendance extends StatelessWidget {
+class ButtonAttendance extends StatefulWidget {
+  final Function onTapAbsen;
+  final Function onTapPulang;
+  final Color backgroundColor;
   ButtonAttendance({
-    @required this.hideFabAnimation,
-    @required this.onTapAttendence,
-    @required this.onTapGoHome,
-    this.bottom = 10,
-    this.left = 10,
-    this.right = 10,
+    this.onTapAbsen,
+    this.onTapPulang,
+    this.backgroundColor = Colors.white,
   });
+  @override
+  _ButtonAttendanceState createState() => _ButtonAttendanceState();
+}
 
-  final AnimationController hideFabAnimation;
-  final Function onTapAttendence;
-  final Function onTapGoHome;
-  final double bottom;
-  final double left;
-  final double right;
+class _ButtonAttendanceState extends State<ButtonAttendance> {
+  DateTime now;
+  @override
+  void initState() {
+    now = DateTime.now();
+    super.initState();
+  }
+
+  Future checkAlreadyAbsent(String idUser) async {
+    final result = absensiAPI.checkAbsenMasukDanPulang(
+        idUser: idUser, tanggalAbsenMasuk: DateTime(now.year, now.month, now.day));
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: bottom,
-      left: left,
-      right: right,
-      child: ScaleTransition(
-        scale: hideFabAnimation,
-        child: Card(
-          // color: Colors.transparent,
-          elevation: 0,
-          child: Column(
-            children: [
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Text('Absen Masuk'),
-                  Text('Absen Pulang'),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Flexible(
-                    child: ButtonCustom(
-                      onPressed: onTapAttendence,
-                      padding: EdgeInsets.symmetric(horizontal: 6.0),
-                      child: LiveClock(),
+    print("Rebuild Button Attendance Screen");
+
+    final user = Provider.of<UserProvider>(context);
+    return FutureBuilder(
+      future: checkAlreadyAbsent(user.user.idUser),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return LinearProgressIndicator();
+        }
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        if (snapshot.hasData) {
+          return Card(
+            color: widget.backgroundColor,
+            elevation: 0,
+            child: Column(
+              children: [
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text('Absen Masuk'),
+                    Text('Absen Pulang'),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Flexible(
+                      child: ButtonCustom(
+                        onPressed: (snapshot.data == 2)
+                            ? null
+                            : (snapshot.data == 1)
+                                ? null
+                                : widget.onTapAbsen ??
+                                    () async {
+                                      await context.read<ZAbsenProvider>().getCurrentPosition();
+                                      await context.read<ZAbsenProvider>().saveDestinasiUser(
+                                          context.read<UserProvider>().user.idUser);
+                                      Navigator.of(context).pushNamed(MapScreen.routeNamed);
+                                    },
+                        padding: EdgeInsets.symmetric(horizontal: 6.0),
+                        child: LiveClock(),
+                      ),
                     ),
-                  ),
-                  Flexible(
-                    child: ButtonCustom(
-                      padding: EdgeInsets.symmetric(horizontal: 6.0),
-                      child: LiveClock(),
-                      onPressed: onTapGoHome,
+                    Flexible(
+                      child: ButtonCustom(
+                        padding: EdgeInsets.symmetric(horizontal: 6.0),
+                        child: LiveClock(),
+                        onPressed: (snapshot.data == 2)
+                            ? null
+                            : (snapshot.data != 1)
+                                ? null
+                                : widget.onTapPulang ??
+                                    () async {
+                                      try {
+                                        await context.read<ZAbsenProvider>().getCurrentPosition();
+                                        await context.read<ZAbsenProvider>().saveDestinasiUser(
+                                            context.read<UserProvider>().user.idUser);
+                                        Navigator.of(context).pushNamed(MapScreen.routeNamed);
+                                      } catch (e) {
+                                        globalF.showToast(message: e, isError: true);
+                                      }
+                                    },
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+        return Text('No Data');
+      },
     );
   }
 }
