647c97cbd32a28f1c9af2fc5b1083f08018adeac
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-11 20:31:48 +0700

    Separated Widget Login Screen

--- lib/src/ui/screens/login_screen.dart
+++ lib/src/ui/screens/login_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:network/network.dart';
-import 'package:provider/provider.dart';
 import 'package:global_template/global_template.dart';
 
-import '../../providers/user_provider.dart';
-import '../screens/welcome_screen.dart';
+import './widgets/login_screen/form.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeNamed = "/login-screen";
@@ -32,108 +29,5 @@ class _LoginScreenState extends State<LoginScreen> with TickerProviderStateMixin
         background: Container(color: colorPallete.primaryColor),
       ),
     );
-  }
-}
-
-class FormUser extends StatefulWidget {
-  final GlobalKey<FormState> formKey;
-  FormUser({@required this.formKey});
-  @override
-  _FormUserState createState() => _FormUserState();
-}
-
-class _FormUserState extends State<FormUser> {
-  String username, password, fullName;
-  @override
-  Widget build(BuildContext context) {
-    final GlobalProvider globalProvider = Provider.of(context);
-    return Column(
-      children: [
-        TextFormFieldCustom(
-          onSaved: (value) => username = value,
-          hintText: '',
-          radius: 50,
-        ),
-        SizedBox(height: 20),
-        TextFormFieldCustom(
-          onSaved: (value) => password = value,
-          isPassword: true,
-          labelText: 'Password',
-          hintText: '',
-          radius: 50,
-          textInputAction: TextInputAction.done,
-        ),
-        SizedBox(height: 20),
-        if (globalProvider.isRegister) ...[
-          TextFormFieldCustom(
-            onSaved: (value) => fullName = value,
-            prefixIcon: Icon(Icons.contacts),
-            labelText: 'Full Name',
-            hintText: '',
-            radius: 50,
-            textInputAction: TextInputAction.done,
-          ),
-          SizedBox(height: 20),
-        ],
-        Consumer<UserProvider>(
-          builder: (_, value, __) => ButtonCustom(
-            onPressed: globalProvider.isLoading
-                ? null
-                : () => _validate(userProvider: value, globalProvider: globalProvider),
-            buttonTitle: globalProvider.isRegister ? " Register" : "Login",
-          ),
-        ),
-        SizedBox(height: 20),
-        OutlineButton(
-          child: Text(globalProvider.isRegister ? "Back To Login" : "Don't Have Fake Account ?"),
-          onPressed: () => globalProvider.setRegister(!globalProvider.isRegister),
-        )
-      ],
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-    );
-  }
-
-  void _validate({
-    @required UserProvider userProvider,
-    @required GlobalProvider globalProvider,
-  }) async {
-    try {
-      if (widget.formKey.currentState.validate()) {
-        print("success validate");
-        globalProvider.setLoading(true);
-        widget.formKey.currentState.save();
-        if (globalProvider.isRegister) {
-          print("ke register");
-          final result = await userApi.userRegister(
-            username: username,
-            password: password,
-            fullName: fullName,
-          );
-
-          globalF.showToast(message: result, isSuccess: true, isLongDuration: true);
-
-          globalProvider.setRegister(false);
-          globalProvider.setLoading(false);
-        } else {
-          print("ke Login");
-
-          final result = await userApi.userLogin(
-            username: username,
-            password: password,
-          );
-          userProvider.saveSessionUser(list: result);
-          Navigator.of(context).pushReplacementNamed(WelcomeScreen.routeNamed);
-          globalProvider.setLoading(false);
-        }
-      } else {
-        print("failed validate");
-        return null;
-      }
-    } catch (e) {
-      print(e.toString());
-      globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
-      globalProvider.setLoading(false);
-    }
   }
 }
