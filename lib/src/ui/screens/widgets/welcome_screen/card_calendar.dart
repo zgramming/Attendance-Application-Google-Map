4ea0a52804e6228d1a6e34284badbd8b03e13fc5
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-12 21:28:23 +0700

    Add List<StatusAbsen>

--- lib/src/ui/screens/widgets/welcome_screen/card_calendar.dart
+++ lib/src/ui/screens/widgets/welcome_screen/card_calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:global_template/global_template.dart';
+import 'package:network/network.dart';
 
 import '../../../../function/zabsen_function.dart';
 
@@ -8,71 +9,79 @@ class CardCalendar extends StatelessWidget {
     Key key,
     @required this.now,
     @required this.index,
+    @required this.list,
   }) : super(key: key);
 
   final DateTime now;
   final int index;
+  final List<AbsensiStatusModel> list;
 
   @override
   Widget build(BuildContext context) {
+    print("Rebuild Card Calendar");
     return SizedBox(
       width: sizes.width(context) / 5.5,
-      child: Card(
-        color: commonF.isWeekend(now, index),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-          side: now.day == index + 1
-              ? BorderSide(color: Colors.green, width: 3)
-              : BorderSide(color: Colors.transparent),
-        ),
-        child: InkWell(
-          onTap: () => '',
-          borderRadius: BorderRadius.circular(15),
-          child: Column(
-            children: [
-              Flexible(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "${index + 1}",
-                    style: appTheme.headline5(context).copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: commonF.isWeekend(
-                            now,
-                            index,
-                            colorWeekend: colorPallete.white,
-                            colorWeekDay: colorPallete.black.withOpacity(.7),
-                          ),
-                        ),
-                  ),
-                ),
-                fit: FlexFit.tight,
-                flex: 3,
-              ),
-              Flexible(
-                fit: FlexFit.tight,
-                child: Text(
-                  globalF.formatDay(
-                    DateTime(
-                      now.year,
-                      now.month,
-                      index + 1,
+      child: Stack(
+        children: [
+          Card(
+            color: commonF.isWeekend(now, index),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+              side: now.day == index + 1
+                  ? BorderSide(color: Colors.green, width: 3)
+                  : BorderSide(color: Colors.transparent),
+            ),
+            child: InkWell(
+              onTap: () => '',
+              borderRadius: BorderRadius.circular(15),
+              child: Column(
+                children: [
+                  Flexible(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "${index + 1}",
+                        style: appTheme.headline5(context).copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: commonF.isWeekend(
+                                now,
+                                index,
+                                colorWeekend: colorPallete.white,
+                                colorWeekDay: colorPallete.black.withOpacity(.7),
+                              ),
+                            ),
+                      ),
                     ),
+                    fit: FlexFit.tight,
+                    flex: 3,
                   ),
-                  textAlign: TextAlign.center,
-                  style: appTheme.caption(context).copyWith(
-                        color: commonF.isWeekend(
-                          now,
-                          index,
-                          colorWeekend: colorPallete.white,
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Text(
+                      globalF.formatDay(
+                        DateTime(
+                          now.year,
+                          now.month,
+                          index + 1,
                         ),
                       ),
-                ),
+                      textAlign: TextAlign.center,
+                      style: appTheme.caption(context).copyWith(
+                            color: commonF.isWeekend(
+                              now,
+                              index,
+                              colorWeekend: colorPallete.white,
+                            ),
+                          ),
+                    ),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.stretch,
               ),
-            ],
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            ),
           ),
-        ),
+          Positioned(child: commonF.setStatusAbsenIcon(list, index), top: 0, right: 0),
+        ],
       ),
     );
   }
