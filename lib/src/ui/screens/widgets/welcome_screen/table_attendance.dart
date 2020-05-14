9849ddcf30da2eabfef018d3999200e115584e83
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-14 23:37:00 +0700

    Implement New Function Provider

--- lib/src/ui/screens/widgets/welcome_screen/table_attendance.dart
+++ lib/src/ui/screens/widgets/welcome_screen/table_attendance.dart
@@ -1,8 +1,34 @@
+import 'package:network/network.dart';
 import 'package:flutter/material.dart';
 import 'package:global_template/global_template.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class TableAttendance extends StatelessWidget {
+import 'package:provider/provider.dart';
+import 'package:z_absen/src/providers/zabsen_provider.dart';
+
+import '../../../../providers/user_provider.dart';
+import './table_attendance_body.dart';
+
+class TableAttendance extends StatefulWidget {
+  @override
+  _TableAttendanceState createState() => _TableAttendanceState();
+}
+
+class _TableAttendanceState extends State<TableAttendance> {
+  Future<List<AbsensiModel>> absensiMonthly;
+  DateTime now;
+  @override
+  void initState() {
+    now = DateTime.now();
+    absensiMonthly = getAbsenMonthly(context.read<UserProvider>().user.idUser, now);
+    super.initState();
+  }
+
+  Future<List<AbsensiModel>> getAbsenMonthly(String idUser, DateTime now) async {
+    final result = absensiAPI.getAbsenMonthly(idUser: idUser, dateTime: now);
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     print("Rebuild Table Attendance");
@@ -49,30 +75,41 @@ class TableAttendance extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                child: ListView.builder(
-                  itemCount: 15,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  padding: const EdgeInsets.all(0),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 10.0, left: 4.0),
-                      child: DefaultTextStyle(
-                        style: appTheme.caption(context).copyWith(fontWeight: FontWeight.bold),
-                        child: Row(
-                          children: [
-                            Flexible(child: Text('Tanggal'), fit: FlexFit.tight, flex: 2),
-                            Flexible(child: Text('Datang'), fit: FlexFit.tight),
-                            Flexible(child: Text('Pulang'), fit: FlexFit.tight),
-                            Flexible(child: Text('Durasi'), fit: FlexFit.tight),
-                          ],
+              Consumer<ZAbsenProvider>(
+                builder: (_, provider, __) => FutureBuilder(
+                  future: provider.fetchAbsenMonthly(context.read<UserProvider>().user.idUser, now),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    print("PANJANG TABLE ATTENDANCE ${provider.tableAttendance.length}");
+                    if (snapshot.connectionState != ConnectionState.done)
+                      return LoadingFutureBuilder(isLinearProgressIndicator: true);
+                    if (snapshot.hasError)
+                      return RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            absensiMonthly =
+                                getAbsenMonthly(context.read<UserProvider>().user.idUser, now);
+                          });
+                        },
+                        child: Text(snapshot.error.toString()),
+                      );
+                    if (snapshot.hasData) {
+                      return Container(
+                        child: ListView.builder(
+                          itemCount: provider.tableAttendance.length,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          padding: const EdgeInsets.all(0),
+                          itemBuilder: (BuildContext context, int index) {
+                            final result = provider.tableAttendance[index];
+                            return TableAttendanceBody(index: index, now: now, result: result);
+                          },
                         ),
-                      ),
-                    );
+                      );
+                    }
+                    return Text('no data');
                   },
                 ),
-              )
+              ),
             ],
           ),
         )
