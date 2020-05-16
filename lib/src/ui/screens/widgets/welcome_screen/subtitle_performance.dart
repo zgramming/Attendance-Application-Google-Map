dc3a207035e7dbcafe60cdbfb0ad40b5b85b2f0d
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-16 23:23:56 +0700

    New Separated Widget SubtitlePerformance

---
+++ lib/src/ui/screens/widgets/welcome_screen/subtitle_performance.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SubtitlePerformance extends StatelessWidget {
+  const SubtitlePerformance({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      fit: FlexFit.tight,
+      child: Row(
+        children: [
+          const Flexible(
+            child: Text(
+              'Hari Kerja',
+              textAlign: TextAlign.center,
+            ),
+            fit: FlexFit.tight,
+          ),
+          const Flexible(
+            child: Text(
+              'Tepat Waktu',
+              textAlign: TextAlign.center,
+            ),
+            fit: FlexFit.tight,
+          ),
+        ],
+      ),
+    );
+  }
+}
