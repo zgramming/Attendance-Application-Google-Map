27ab5e36a104c54b6b56c852d9e3a87c2bff5f58
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-24 00:18:54 +0700

    Add Destination Drawer Menu

--- lib/src/ui/screens/widgets/drawer/drawer_body.dart
+++ lib/src/ui/screens/widgets/drawer/drawer_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:global_template/global_template.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import './drawer_body_menu.dart';
@@ -8,7 +9,9 @@ import './drawer_body_menu_absen.dart';
 
 import '../../login_screen.dart';
 import '../../user_profil_screen.dart';
+import '../../add_destination_screen.dart';
 
+import '../../../../providers/zabsen_provider.dart';
 import '../../../../providers/user_provider.dart';
 
 class DrawerBody extends StatelessWidget {
@@ -24,13 +27,18 @@ class DrawerBody extends StatelessWidget {
         DrawerBodyTitle(title: "Absen"),
         DrawerBodyMenuAbsen(),
         DrawerBodyTitle(title: "Destinasi"),
-        DrawerBodyMenu(
-          icon: FontAwesomeIcons.fortAwesome,
-          subtitle: "Tambah Destinasi",
+        Selector<GlobalProvider, bool>(
+          selector: (_, provider) => provider.isLoading,
+          builder: (_, isLoading, __) => DrawerBodyMenu(
+            icon: FontAwesomeIcons.fortAwesome,
+            subtitle: isLoading ? "Loading..." : "Tambah Destinasi",
+            onTap: isLoading ? null : () => goToAddDestination(context),
+          ),
         ),
         DrawerBodyMenu(
           icon: FontAwesomeIcons.mapMarkerAlt,
-          subtitle: "Pilih Destinasi",
+          subtitle: "Pilih Destinasi ",
+          onTap: () => Navigator.of(context).pushNamed(AddDestinationScreen.routeNamed),
         ),
         DrawerBodyTitle(title: "Akun"),
         DrawerBodyMenu(
@@ -63,5 +71,24 @@ class DrawerBody extends StatelessWidget {
       ],
       crossAxisAlignment: CrossAxisAlignment.stretch,
     );
+  }
+
+  void goToAddDestination(BuildContext context) async {
+    //! Membuat Button Menjadi Disable , Untuk Prevent Double Click
+    context.read<GlobalProvider>().setLoading(true);
+    // Future.delayed(Duration(seconds: 4), () => context.read<GlobalProvider>().setLoading(false));
+    try {
+      print('Proses Mendapatkan Initial Position');
+      await context.read<ZAbsenProvider>().getCurrentPosition();
+      print('Proses Menyimpan Destinasi User');
+      await context
+          .read<ZAbsenProvider>()
+          .saveDestinasiUser(context.read<UserProvider>().user.idUser)
+          .then((_) => context.read<GlobalProvider>().setLoading(false))
+          .then((_) => Navigator.of(context).pushNamed(AddDestinationScreen.routeNamed));
+    } catch (e) {
+      globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
+      context.read<GlobalProvider>().setLoading(false);
+    }
   }
 }
