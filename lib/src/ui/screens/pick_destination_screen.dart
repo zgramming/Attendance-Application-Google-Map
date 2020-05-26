28b8a5d892b7b41da472855eaf713e9c6a2c63bd
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-26 21:47:53 +0700

    Added New Function Filter Destination By Name

--- lib/src/ui/screens/pick_destination_screen.dart
+++ lib/src/ui/screens/pick_destination_screen.dart
@@ -1,11 +1,24 @@
+import 'package:tuple/tuple.dart';
+import 'package:network/network.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:global_template/global_template.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import './widgets/pick_destination_screen/picked_destination.dart';
+import './widgets/pick_destination_screen/list_destination.dart';
+
+import '../../providers/absen_provider.dart';
+import '../../providers/user_provider.dart';
 
 class PickDestinationScreen extends StatelessWidget {
   static const routeNamed = "/pick-destination-screen";
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _filterController = TextEditingController();
+    print('Rebuild Pick Destination Screen');
+    final absenProvider = Provider.of<AbsenProvider>(context, listen: false);
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: Text('Pilih Destinasi')),
       body: Column(
@@ -14,46 +27,61 @@ class PickDestinationScreen extends StatelessWidget {
           Align(
             alignment: Alignment.center,
             child: Padding(
-              padding: const EdgeInsets.only(top: kToolbarHeight / 2, left: 40, right: 40),
+              padding:
+                  const EdgeInsets.only(top: kToolbarHeight / 2, left: 20, right: 20, bottom: 20),
               child: TextFormFieldCustom(
                 onSaved: (value) => '',
                 disableOutlineBorder: false,
                 prefixIcon: Icon(FontAwesomeIcons.searchLocation),
                 hintText: "Cari Destinasi ...",
+                controller: _filterController,
+                onChanged: (query) => context.read<AbsenProvider>().filterListDestination(query),
+                suffixIcon: IconButton(
+                  icon: Icon(FontAwesomeIcons.times),
+                  onPressed: () {
+                    _filterController.clear();
+                    context.read<AbsenProvider>().resetFilterListDestination();
+                  },
+                  iconSize: 18,
+                ),
               ),
             ),
           ),
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Text('data'),
-                ],
-              ),
+            child: FutureBuilder(
+              future: absenProvider.fetchDestinationUser(userProvider.user.idUser),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return LoadingFutureBuilder();
+                }
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                }
+                return Selector3<AbsenProvider, AbsenProvider, GlobalProvider,
+                    Tuple3<List<DestinasiModel>, List<DestinasiModel>, bool>>(
+                  selector: (_, fullList, filteredList, isLoading) => Tuple3(fullList.listDestinasi,
+                      filteredList.filteredListDestinasi, isLoading.isLoading),
+                  builder: (_, value, __) {
+                    print("Rebuild Selector Pick Destination Screen");
+                    final resultList =
+                        (value.item2.length != 0 || _filterController.text.isNotEmpty)
+                            ? value.item2
+                            : value.item1.where((element) => element.status != "t").toList();
+                    return value.item3
+                        ? LoadingFutureBuilder(isLinearProgressIndicator: false)
+                        : ListView.builder(
+                            itemCount: resultList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final result = resultList[index];
+                              return ListDestination(result: result);
+                            },
+                          );
+                  },
+                );
+              },
             ),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(30),
-              ),
-              color: colorPallete.white,
-              boxShadow: [
-                BoxShadow(color: Colors.black87, blurRadius: 3),
-              ],
-            ),
-            child: Column(
-              children: [
-                Text(
-                  'Destinasimu',
-                  style:
-                      appTheme.headline6(context).copyWith(fontFamily: "Righteous", fontSize: 18),
-                ),
-              ],
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-            ),
-          )
+          PickedDestination()
         ],
       ),
     );
