00741b138449b5eb843f2a2b443f04361685bfbf
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-05-28 19:18:49 +0700

    Separated Widget

--- lib/src/ui/screens/add_destination_screen.dart
+++ lib/src/ui/screens/add_destination_screen.dart
@@ -8,9 +8,9 @@ import 'package:global_template/global_template.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import './widgets/pick_destination_screen/add_destination_form.dart';
+
 import '../../providers/maps_provider.dart';
-import '../../providers/user_provider.dart';
-import '../../providers/absen_provider.dart';
 
 class AddDestinationScreen extends StatefulWidget {
   static const routeNamed = "/add-destination-screen";
@@ -38,7 +38,7 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tambah Destinasi'),
+        title: const Text('Tambah Destinasi'),
         actions: [
           InkWell(
             onTap: () => showModalBottomSheet(
@@ -47,7 +47,7 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
                 nameDestinationController: _nameDestinationController,
               ),
             ),
-            child: Padding(
+            child: const Padding(
               padding: const EdgeInsets.only(right: 8.0),
               child: Icon(
                 FontAwesomeIcons.check,
@@ -70,7 +70,8 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
                 _controller.complete(controller);
                 await _gotToCenterUser();
               },
-              onCameraMove: (position) => context.read<MapsProvider>().setCameraPosition(position),
+              onCameraMove: (position) =>
+                  context.read<MapsProvider>().setTrackingCameraPosition(position),
               onCameraIdle: () => print("Stop Camera"),
             ),
           ),
@@ -149,87 +150,6 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
       print(err);
     } catch (e) {
       globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
-    }
-  }
-}
-
-class AddDestinationForm extends StatelessWidget {
-  const AddDestinationForm({
-    Key key,
-    @required TextEditingController nameDestinationController,
-  })  : _nameDestinationController = nameDestinationController,
-        super(key: key);
-
-  final TextEditingController _nameDestinationController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormFieldCustom(
-            controller: _nameDestinationController,
-            onSaved: (value) => '',
-            prefixIcon: Icon(FontAwesomeIcons.fortAwesome),
-            hintText: "Nama Destinasi",
-            autoFocus: true,
-          ),
-          Row(
-            children: [
-              Spacer(),
-              FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('Batal')),
-              Selector<GlobalProvider, bool>(
-                selector: (_, provider) => provider.isLoading,
-                builder: (_, isLoading, __) => Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: isLoading
-                      ? LoadingFutureBuilder(isLinearProgressIndicator: false)
-                      : FlatButton(
-                          onPressed: () => _destinationRegister(context),
-                          child: Text('Simpan'),
-                          color: colorPallete.primaryColor,
-                          textTheme: ButtonTextTheme.primary,
-                        ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  void _destinationRegister(BuildContext context) async {
-    final userProvider = context.read<UserProvider>();
-    final mapsProvider = context.read<MapsProvider>();
-    final absenProvider = context.read<AbsenProvider>();
-    final globalProvider = context.read<GlobalProvider>();
-    if (mapsProvider.cameraPosition == null) {
-      globalF.showToast(message: "Belum Memilih Destinasi", isError: true, isLongDuration: true);
-    } else if (_nameDestinationController.text.isEmpty) {
-      globalF.showToast(message: "Nama Destinasi Belum Diisi", isError: true, isLongDuration: true);
-    } else {
-      try {
-        globalProvider.setLoading(true);
-        print("Proses Add Destination");
-        final result = await absenProvider.destinationRegister(
-          idUser: userProvider.user.idUser,
-          nameDestination: _nameDestinationController.text,
-          latitude: mapsProvider.cameraPosition.target.latitude,
-          longitude: mapsProvider.cameraPosition.target.longitude,
-        );
-        print("Selesai Add Destination");
-        globalF.showToast(message: result, isSuccess: true, isLongDuration: true);
-        _nameDestinationController.clear();
-        globalProvider.setLoading(false);
-
-        Navigator.of(context).pop();
-      } catch (e) {
-        globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
-        globalProvider.setLoading(false);
-      }
     }
   }
 }
