87759273569ee8c9dd0eb3634fc68ea43751858c
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-06-02 17:15:41 +0700

    Cleaning Code

--- lib/src/ui/screens/splash_screen.dart
+++ lib/src/ui/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:global_template/global_template.dart';
+
 import './welcome_screen.dart';
 import './login_screen.dart';
 import '../../providers/user_provider.dart';
