03494371c8f4f1d800e3f131b3061d1cf87f7ef6
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-06-27 21:21:26 +0700

    Adding language and country code Indonesia

--- global_template/lib/variable/config/app_config.dart
+++ global_template/lib/variable/config/app_config.dart
@@ -4,7 +4,7 @@ class AppConfig {
   final Map<String, String> headersApi = {'Content-Type': 'application/x-www-form-urlencoded'};
 
   final String baseImageApiUrl = 'http://www.zimprov.id/absensi_online/images';
-  final String baseApiUrl = 'http://www.zimprov.id/absensi_online/api/';
+  final String baseApiUrl = 'http://www.zimprov.id/absensi_online/api';
 
   final String indonesiaLocale = 'id_ID';
 
@@ -15,6 +15,9 @@ class AppConfig {
   static const defaultImageNetwork = 'https://flutter.io/images/catalog-widget-placeholder.png';
   static const imageLogoAsset = 'assets/images/logo.png';
   static const emptyDestination = 'assets/images/empty_destination.png';
+
+  static const languageID = 'id';
+  static const countryCodeID = 'ID';
 }
 
 final appConfig = AppConfig();
